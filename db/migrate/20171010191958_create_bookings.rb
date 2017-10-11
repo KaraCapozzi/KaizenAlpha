4be058be3ac2aaee95721482b53eec1fd27f640b
sakin <egalsakin@gmail.com>
2017-10-11 19:18:38 -0400

    dropped table, migrated fixed the whole database

--- db/migrate/20171010191958_create_bookings.rb
+++ db/migrate/20171010191958_create_bookings.rb
@@ -3,8 +3,9 @@ class CreateBookings < ActiveRecord::Migration[5.1]
     create_table :bookings do |t|
       t.integer :mentor_id
       t.integer :student_id
-      t.string :date
+      t.datetime :booking_date
       t.string :status
+      t.time   :booking_time
 
       t.timestamps
     end
