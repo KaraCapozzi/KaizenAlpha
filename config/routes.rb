752efce4b5cdbbb0a7e3ecf1f6bb31316e0f5625
sakin <egalsakin@gmail.com>
2017-10-12 16:24:40 -0400

    create log in | create sessions controller | create log in form | create log in and log out forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  get 'sessions/new'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#show'
+
+  resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
