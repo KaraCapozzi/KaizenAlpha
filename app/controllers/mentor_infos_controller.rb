9613f24441a3111d933b7795842c742208cc4031
sakin <egalsakin@gmail.com>
2017-10-13 18:02:01 -0400

    mentor_info params edit

--- app/controllers/mentor_infos_controller.rb
+++ app/controllers/mentor_infos_controller.rb
@@ -12,7 +12,11 @@ class MentorInfosController < ApplicationController
 
   def create
     user = current_user
-    # user.create_mentor_info(summary: params[:mentor_info][:summary], location: params[:mentor_info][:location])
+    if mentor_info = nil
+      user.create_mentor_info(summary: params[:mentor_info][:summary], location: params[:mentor_info][:location])
+    else
+      mento_info can edit their profile 
+    end
     subcat_add = Subcategory.where(id: params[:subcategories].keys)
     subcat_add.each do |subcat|
       user.subcategories << subcat
